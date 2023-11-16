8f69ad0122a3a7d2f74e56227838867162e37cb5
mango <esudatcom@gmail.com>
2023-11-16 16:16:38 +0900

    feat: Turn 을 증가시키는 turnPrint 메서드 구현

--- src/main/java/puzzle/Step1.java
+++ src/main/java/puzzle/Step1.java
@@ -3,13 +3,20 @@ package puzzle;
 // 원래보다 간단한 기능의 숫자퍼즐 구현
 public class Step1 {
     private static final String TURN_PREFIX = "Turn ";
+    private static int turn = 1 ;
 
     public static void main(String[] args) {
         System.out.println("간단 숫자 퍼즐");
+        turnPrint();
 
     }
 
-    public static int turnPrint (){
+    public static void turnPrint (){
+        System.out.println(TURN_PREFIX + turn);
+        turn ++;
+    }
+
+    public static void generateNumbers(){
 
     }
 
