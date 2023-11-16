2cc0f407c98fb835d1933273d850193471073e86
mango <esudatcom@gmail.com>
2023-11-16 14:34:52 +0900

    docs: 숫자 퍼즐 1단계 기능 목록 README 작성

---
+++ src/main/java/puzzle/Application.java
@@ -0,0 +1,7 @@
+package puzzle;
+
+public class Application {
+    public static void main(String[] args) {
+        // 코드 구현
+    }
+}
---
+++ src/main/java/puzzle/Step1.java
@@ -0,0 +1,17 @@
+package puzzle;
+
+// 원래보다 간단한 기능의 숫자퍼즐 구현
+public class Step1 {
+    private static final String TURN_PREFIX = "Turn ";
+
+    public static void main(String[] args) {
+        System.out.println("간단 숫자 퍼즐");
+
+    }
+
+    public static int turnPrint (){
+
+    }
+
+
+}
