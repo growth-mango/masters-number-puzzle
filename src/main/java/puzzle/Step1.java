80c01ce294a81ee7fda4aef0838fe49eddbb2dae
mango <esudatcom@gmail.com>
2023-11-16 17:52:58 +0900

    feat: 8개의 숫자를 랜덤하게 생성하는 generateNumbers 메서드 구현

--- src/main/java/puzzle/Step1.java
+++ src/main/java/puzzle/Step1.java
@@ -1,9 +1,13 @@
 package puzzle;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 // 원래보다 간단한 기능의 숫자퍼즐 구현
 public class Step1 {
     private static final String TURN_PREFIX = "Turn ";
-    private static int turn = 1 ;
+    private static int turn = 1;
 
     public static void main(String[] args) {
         System.out.println("간단 숫자 퍼즐");
@@ -11,13 +15,18 @@ public class Step1 {
 
     }
 
-    public static void turnPrint (){
+    public static void turnPrint() {
         System.out.println(TURN_PREFIX + turn);
-        turn ++;
+        turn++;
     }
 
-    public static void generateNumbers(){
-
+    public static List<Integer> generateNumbers() {
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i <= 8; i++) {
+            numbers.add(i);
+        }
+        Collections.shuffle(numbers);
+        return numbers;
     }
 
 
