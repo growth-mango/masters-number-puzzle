140af65ac131550a4e387c4f49269f4aedcdfb7a
mango <esudatcom@gmail.com>
2023-11-16 18:34:40 +0900

    feat: 2개의 숫자를 입력받는 getUserInput 메서드 구현

--- src/main/java/puzzle/Application.java
+++
@@ -1,7 +0,0 @@
-package puzzle;
-
-public class Application {
-    public static void main(String[] args) {
-        // 코드 구현
-    }
-}
--- src/main/java/puzzle/Step1.java
+++ src/main/java/puzzle/Step1.java
@@ -1,8 +1,6 @@
 package puzzle;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 // 원래보다 간단한 기능의 숫자퍼즐 구현
 public class Step1 {
@@ -11,7 +9,12 @@ public class Step1 {
 
     public static void main(String[] args) {
         System.out.println("간단 숫자 퍼즐");
+        //임시 테스트
         turnPrint();
+        List<Integer> radomNumbers = generateNumbers();
+        System.out.println(radomNumbers);
+        int[] inputNumbers = getUserInput();
+        System.out.println(Arrays.toString(inputNumbers));
 
     }
 
@@ -29,5 +32,27 @@ public class Step1 {
         return numbers;
     }
 
+    public static int[] getUserInput() {
+        Scanner scanner = new Scanner(System.in);
+        int[] inputNumbers = new int[2];
+        while (true) {
+            System.out.println("교환할 두 숫자를 입력>");
+            String inputStr = scanner.nextLine();
+            try {
+                String[] parts = inputStr.split(",");
+                if (parts.length == 2) {
+                    inputNumbers[0] = Integer.parseInt(parts[0].trim());
+                    inputNumbers[1] = Integer.parseInt(parts[1].trim());
+                    if (inputNumbers[0] >= 1 && inputNumbers[0] <= 8 && inputNumbers[1] >= 1 && inputNumbers[1] <= 8) {
+                        break;
+                    }
+                }
+                System.out.println("잘못된 입력입니다. 다시 입력해주세요.");
+            } catch (NumberFormatException e) {
+                System.out.println("잘못된 입력입니다. 숫자를 입력해주세요.");
+            }
+        }
+        return inputNumbers;
+    }
 
 }
