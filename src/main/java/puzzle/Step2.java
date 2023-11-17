4d022d079b1106009d272309cff230510bac8f45
mango <esudatcom@gmail.com>
2023-11-17 20:35:19 +0900

    feat: 15개의 숫자를 생성하는 generateNumbers 메서드와 무작위로 섞는 shuffleGrid 메서드 구현

--- src/main/java/puzzle/Step2.java
+++ src/main/java/puzzle/Step2.java
@@ -9,6 +9,9 @@ public class Step2 {
     public static void main(String[] args) {
         System.out.println("재미있는 15 퍼즐!\n");
         turnNumber();
+        System.out.println(Arrays.deepToString(generateNumbers()));
+        int[][] numbers = generateNumbers();
+        shuffleGrid(numbers);
 
     }
 
@@ -27,6 +30,37 @@ public class Step2 {
 //        Collections.shuffle(numbers);
 //        return numbers;
 //    }
+    // 1부터 15까지 숫자 생성 후 배치
+    public static int[][] generateNumbers() {
+        int[][] grid = new int[4][4];
+        int num = 1;
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                grid[i][j] = num <= 15 ? num : 0;
+                num++;
+            }
+        }
+        shuffleGrid(grid);
+        return grid;
+    }
+
+    // 2차원 배열 섞기
+    private static void shuffleGrid(int[][] grid){
+        Random rand = new Random();
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                int i1 = rand.nextInt(4);
+                int j1 = rand.nextInt(4);
+
+                int temp = grid[i][j];
+                grid[i][j] = grid[i1][j1];
+                grid[i1][j1] = temp ;
+            }
+        }
+    }
+
+
 
 
 
